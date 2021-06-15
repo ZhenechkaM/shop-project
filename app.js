6abd66fdc0741497a61e0df3f359ec14327535f2
Matvienko <e.motko@gmail.com>
2021-06-15 14:47:29 +0200

    added app.js, added a like/dislike to the product range

---
+++ app.js
@@ -0,0 +1,30 @@
+// let productsCountEl = document.getElementById("products-count");
+// // console.log(productsCountEl)
+
+// let addToCartButtons = document.querySelectorAll(".btn");
+// // console.log(addToCartButtons)
+
+// for(let i=0; i < addToCartButtons.length; i++) {
+//     addToCartButtons[i].addEventListener("click",function(){
+//         // let prevProductsCount = +productsCountEl.textContent;
+//         // productsCountEl.textContent = prevProductsCount + 1;
+//         productsCountEl.textContent = +productsCountEl.textContent + 1;
+//     })
+// }
+
+
+// let modal=document.querySelector(".modal");
+// let moreDetailsButtons = document.querySelectorAll(".button-more");
+// let closeBtn = document.querySelector(".btn-close");
+// // console.log(modal)
+// // console.log()
+
+// moreDetailsButtons.forEach(item => {
+//     item
+//     modal.classList.add("show");
+//     modal.classList.remove("hide");
+// })
+
+function myFunction(x) {
+    x.classList.toggle("heart_white");
+  }
